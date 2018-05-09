7a6a09419df3c81b0826a811b7b1f7cf808f248b
ALWAYS1996 <danivc@hotmail.es>
2018-05-09 01:40:56 -0600

    Cart system, products, links and so others added

---
+++ js/content.js
@@ -0,0 +1,178 @@
+var contenido ={
+	'hyperx-cloud-revolver-s':{
+		'name':"Cloud Revolver", 
+		'image':"",
+		'brand':"HyperX",
+		'price' : 18000
+	},
+
+	'marvo-hg8952-silver':{
+		'name':"HG8952",
+		'image':"",
+		'brand':"Marvo",
+		'price' : 22000
+	},
+
+	'msi-ds502':{
+		'name':"DS502",
+		'image':"",
+		'brand':"ReDragon",
+		'price' : 25000
+	},
+
+	'razer-mano-war':{
+		'name':"ManoWar",
+		'image':"",
+		'brand':"Razer",
+		'price' : 60000
+	},
+
+	'sennheiser-gsp-303':{
+		'name':"GSP 303",
+		'image':"",
+		'brand':"Sennheiser",
+		'price' : 33000
+	},
+
+	'corsair-k63-wireless':{
+		'name':"K63",
+		'image':"",
+		'brand':"Corsair",
+		'price' : 55000
+	},
+
+	'razer-ornata-chroma':{
+		'name':"Ornata Chroma",
+		'image':"",
+		'brand':"Razer",
+		'price' : 80000
+	},
+
+	'redragon-k503-harpe-rgb':{
+		'name':"H503 Harpe",
+		'image':"",
+		'brand':"ReDragon",
+		'price' : 10000
+	},
+
+	'redragon-k552-kumara':{
+		'name':"H522 Kumara",
+		'image':"",
+		'brand':"ReDragon",
+		'price' : 25000
+	},
+
+	'redragon-k556-rk-devarajas-retro':{
+		'name':"K566 Devarajas",
+		'image':"",
+		'brand':"ReDragon",
+		'price' : 40000
+	},
+
+	'acer-ed242qr-144-hz':{
+		'name':"ED242",
+		'image':"",
+		'brand':"Acer",
+		'price' : 110000
+	},
+
+	'acer-v206hql':{
+		'name':"V206",
+		'image':"",
+		'brand':"Acer",
+		'price' : 130000
+	},
+
+	'aoc-e2275swj':{
+		'name':"E2275",
+		'image':"",
+		'brand':"AOC",
+		'price' : 125000
+	},
+
+	'asus-rog-swift-pg278qr':{
+		'name':"PG278",
+		'image':"",
+		'brand':"Asus",
+		'price' : 150000
+	},
+
+	'asus-vp247qg':{
+		'name':"VP247",
+		'image':"",
+		'brand':"Asus",
+		'price' : 180000
+	},
+
+	'genius-gx-maurus':{
+		'name':"Maurus",
+		'image':"",
+		'brand':"Genius",
+		'price' : 18000
+	},
+
+	'logitech-g903-lightspeed':{
+		'name':"G903",
+		'image':"",
+		'brand':"Logitech",
+		'price' : 22000
+	},
+
+	'marvo-m212':{
+		'name':"M212",
+		'image':"",
+		'brand':"Marvo",
+		'price' : 30000
+	},
+
+	'razer-abyssus-v2':{
+		'name':"Abyssus",
+		'image':"",
+		'brand':"Razer",
+		'price' : 62000
+	},
+
+	'redragon-m901-perdition':{
+		'name':"M901 Perdition",
+		'image':"",
+		'brand':"ReDragon",
+		'price' : 25000
+	},
+
+	'xtremepc-level-1-intel':{
+		'name':"XtremePC LVL 1",
+		'image':"",
+		'brand':"Intel",
+		'price' : 260000
+	},
+
+	'xtremepc-vega':{
+		'name':"XtremePC Vega",
+		'image':"",
+		'brand':"AMD",
+		'price' : 300000
+	},
+
+	'xtremepc-vega-5':{
+		'name':"XtremePC Vega 5",
+		'image':"",
+		'brand':"AMD",
+		'price' : 320000
+	},
+
+	'xtremepc-vega-elite':{
+		'name':"XtremePC Vega Elite",
+		'image':"",
+		'brand':"AMD",
+		'price' : 380000
+	},
+
+	'xtremepc-vega-elite-3':{
+		'name':"XtremePC Vega Elite 3",
+		'image':"",
+		'brand':"AMD",
+		'price' : 420000
+	}
+}
+
+//00webhost
--- js/script.js
+++ js/script.js
@@ -1,42 +1,222 @@
-var contenido ={
-	'hxcrs':{
+//Dynamic site content
+
+var content ={
+	'hyperx-cloud-revolver-s':{
 		'name':"Cloud Revolver", 
-		'image':"",
+		'image':"img/headset/hyperx-cloud-revolver-s.jpg",
 		'brand':"HyperX",
-		'price' : 18000, 
+		'price' : 18000
 	},
 
-	'mhgs':{
+	'marvo-hg8952-silver':{
 		'name':"HG8952",
-		'image':"",
+		'image':"img/headset/marvo-hg8952-silver.jpg",
 		'brand':"Marvo",
-		'price' : 22000,
+		'price' : 22000
 	},
 
-	'msids':{
+	'msi-ds502':{
 		'name':"DS502",
-		'image':"",
+		'image':"img/headset/msi-ds502.jpg",
 		'brand':"ReDragon",
-		'price' : 25000,
+		'price' : 25000
 	},
 
-	'rzmw':{
+	'razer-mano-war':{
 		'name':"ManoWar",
-		'image':"",
+		'image':"img/headset/razer-mano-war.jpg",
 		'brand':"Razer",
-		'price' : 60000,
+		'price' : 60000
 	},
 
-	'shgsp':{
+	'sennheiser-gsp-303':{
 		'name':"GSP 303",
-		'image':"",
+		'image':"img/headset/sennheiser-gsp-303.jpg",
 		'brand':"Sennheiser",
-		'price' : 33000,
+		'price' : 33000
+	},
+
+	'corsair-k63-wireless':{
+		'name':"K63",
+		'image':"img/keyboard/corsair-k63-wireless.jpg",
+		'brand':"Corsair",
+		'price' : 55000
+	},
+
+	'razer-ornata-chroma':{
+		'name':"Ornata Chroma",
+		'image':"img/keyboard/razer-ornata-chroma.jpg",
+		'brand':"Razer",
+		'price' : 80000
+	},
+
+	'redragon-k503-harpe-rgb':{
+		'name':"H503 Harpe",
+		'image':"img/keyboard/redragon-k503-harpe-rgb.jpg",
+		'brand':"ReDragon",
+		'price' : 10000
+	},
+
+	'redragon-k552-kumara':{
+		'name':"H522 Kumara",
+		'image':"img/keyboard/redragon-k552-kumara.jpg",
+		'brand':"ReDragon",
+		'price' : 25000
+	},
+
+	'redragon-k556-rk-devarajas-retro':{
+		'name':"K566 Devarajas",
+		'image':"img/keyboard/redragon-k556-rk-devarajas-retro.jpg",
+		'brand':"ReDragon",
+		'price' : 40000
+	},
+
+	'acer-ed242qr-144-hz':{
+		'name':"ED242",
+		'image':"img/monitor/acer-ed242qr-144-hz.jpg",
+		'brand':"Acer",
+		'price' : 110000
+	},
+
+	'acer-v206hql':{
+		'name':"V206",
+		'image':"img/monitor/acer-v206hql.jpg",
+		'brand':"Acer",
+		'price' : 130000
+	},
+
+	'aoc-e2275swj':{
+		'name':"E2275",
+		'image':"img/monitor/aoc-e2275swj.jpg",
+		'brand':"AOC",
+		'price' : 125000
+	},
+
+	'asus-rog-swift-pg278qr':{
+		'name':"PG278",
+		'image':"img/monitor/asus-rog-swift-pg278qr.jpg",
+		'brand':"Asus",
+		'price' : 150000
+	},
+
+	'asus-vp247qg':{
+		'name':"VP247",
+		'image':"img/monitor/asus-vp247qg.jpg",
+		'brand':"Asus",
+		'price' : 180000
+	},
+
+	'genius-gx-maurus':{
+		'name':"Maurus",
+		'image':"img/mouse/genius-gx-maurus.jpg",
+		'brand':"Genius",
+		'price' : 18000
+	},
+
+	'logitech-g903-lightspeed':{
+		'name':"G903",
+		'image':"img/mouse/logitech-g903-lightspeed.jpg",
+		'brand':"Logitech",
+		'price' : 22000
+	},
+
+	'marvo-m212':{
+		'name':"M212",
+		'image':"img/mouse/marvo-m212.jpg",
+		'brand':"Marvo",
+		'price' : 30000
+	},
+
+	'razer-abyssus-v2':{
+		'name':"Abyssus",
+		'image':"img/mouse/razer-abyssus-v2.jpg",
+		'brand':"Razer",
+		'price' : 62000
+	},
+
+	'redragon-m901-perdition':{
+		'name':"M901 Perdition",
+		'image':"img/mouse/redragon-m901-perdition.jpg",
+		'brand':"ReDragon",
+		'price' : 25000
+	},
+
+	'xtremepc-level-1-intel':{
+		'name':"XtremePC LVL 1",
+		'image':"img/pc/xtremepc-level-1-intel.jpg",
+		'brand':"Intel",
+		'price' : 260000
+	},
+
+	'xtremepc-vega':{
+		'name':"XtremePC Vega",
+		'image':"img/pc/xtremepc-vega.jpg",
+		'brand':"AMD",
+		'price' : 300000
+	},
+
+	'xtremepc-vega-5':{
+		'name':"XtremePC Vega 5",
+		'image':"img/pc/xtremepc-vega-5.jpg",
+		'brand':"AMD",
+		'price' : 320000
+	},
+
+	'xtremepc-vega-elite':{
+		'name':"XtremePC Vega Elite",
+		'image':"img/pc/xtremepc-vega-elite.jpg",
+		'brand':"AMD",
+		'price' : 380000
+	},
+
+	'xtremepc-vega-elite-3':{
+		'name':"XtremePC Vega Elite 3",
+		'image':"img/pc/xtremepc-vega-elite-3.jpg",
+		'brand':"AMD",
+		'price' : 420000
 	}
 }
 
+var contentKeys = ['hyperx-cloud-revolver-s','marvo-hg8952-silver','msi-ds502', 'razer-mano-war', 
+'sennheiser-gsp-303','corsair-k63-wireless','razer-ornata-chroma','redragon-k503-harpe-rgb', 
+'redragon-k552-kumara','redragon-k556-rk-devarajas-retro','acer-ed242qr-144-hz', 'acer-v206hql',
+'aoc-e2275swj','asus-rog-swift-pg278qr','asus-vp247qg','genius-gx-maurus','logitech-g903-lightspeed',
+'marvo-m212','razer-abyssus-v2','redragon-m901-perdition','xtremepc-level-1-intel','xtremepc-vega',
+'xtremepc-vega-5','xtremepc-vega-elite','xtremepc-vega-elite-3']
+
+var selection = window.location.search.substr(1)
+
+//Methods
+
 function generateAds(){
-	document.getElementById('ads').innerHTML = '<img alt="add" src="img/ads/' + Math.floor((Math.random())+1)+ '.jpg">'
+	document.getElementById('ads').innerHTML = '<img alt="add" src="img/ads/' + Math.floor((Math.random())+1) + '.jpg">'
+}
+
+function generateCart(){
+	var text = ""
+	if(localStorage.length != 0){
+		for(var i = 0; i < localStorage.length; i++){
+		text += '<div id="cart"> <image class="cartImg" src="'+ content[localStorage.key(i)]["image"] +'">'
+		+'<div id="cart"><label>'+ content[localStorage.key(i)]["brand"] + ': ' + content[localStorage.key(i)]["name"] +'</label></div>'
+		+'<div id="cart"><label>Quantity ' + localStorage.getItem(localStorage.key(i)) + '</label></div>'
+		+'<div id="cart"><label>Price ' + price(i) + '</label></div>'
+		+'<div id="cart"><button id="cartButton" onclick="removeItemFromCart(this)" title="' + localStorage.key(i) + '">Remove</button></div></div>'
+		}
+		text += '<label>Prices with * have a total 20% discount</label>'	
+	}
+	else{
+		text = "<label>No items in cart yet</label>"
+	}
+	return text
+}
+
+function price(i){
+	if(Number(localStorage.getItem(localStorage.key(i))) >= 3){
+		return (Number(content[localStorage.key(i)]['price']) * Number(localStorage.getItem(localStorage.key(i))))* 0.80 + "*"
+	}
+	else{
+		return (Number(content[localStorage.key(i)]['price']) * Number(localStorage.getItem(localStorage.key(i))))
+	}
 }
 
 function openModal(target){
@@ -50,20 +230,56 @@ function closeModal(){
 }
 
 function generateContent(){
-	var selection = window.location.search.substr(1)
-	if(selection == ""){
+	var text = ""
+
+	if(selection == "" || selection == "promotions" || selection == "contact"){
 		document.getElementById('main').innerHTML = ""
 	}
 
-	else if(selection == "promotions" || selection == "contact"){
-		document.getElementById('container').innerHTML = ""
+	else if(selection == "mouse" || selection == "keyboard" || selection == "monitor" || selection == "headset" || selection == "pc"){
+		for(var i = 0; i < contentKeys.length; i++){
+			if(content[contentKeys[i]]['image'].includes(selection)){
+				text += '<div id="cart"><img class="cartImg" src=' + content[contentKeys[i]]['image'] + '>'
+				+'<div id="cart"><label><a href="?' + contentKeys[i] + '">' + content[contentKeys[i]]['name']+ '</a></label></div></div>'
+			}
+		}
+		document.getElementById('main').innerHTML = text
+	}
+
+	else if(selection == "cart"){
+		document.getElementById('main').innerHTML = generateCart()
 	}
 
 	else{
-		document.getElementById('name').innerHTML = contenido[selection]['name']
-		document.getElementById('price').innerHTML = contenido[selection]['price']
-		document.getElementById('description').innerHTML = contenido[selection]['description']
+		document.getElementById('name').innerHTML = content[selection]['name']
+		document.getElementById('price').innerHTML = content[selection]['price']
+		document.getElementById('brand').innerHTML = content[selection]['brand']
+		document.getElementById('image').setAttribute('src', content[selection]['image'])
+		document.getElementById('addToCart').setAttribute('title', selection)
 	}
 }
 
-//mediaQuery para ser responsive
+function addItemToCart(target){
+	if(!localStorage.getItem(target.getAttribute('title'))){
+		localStorage.setItem(target.getAttribute('title'), 1)
+	}else{
+		localStorage.setItem(target.getAttribute('title'), Number(localStorage.getItem(target.getAttribute('title'))) + 1) 
+	}
+	window.location.replace("?cart")
+}
+
+function removeItemFromCart(target){
+	if(!localStorage.getItem(target.getAttribute('title'))){
+		alert("Page must be reloaded for changes to be maded")
+	}
+	else{
+		localStorage.removeItem(target.getAttribute('title'))
+		location.reload()
+	}
+}
+
+//Storage
+
+if(typeof(Storage) == "undefined"){
+	alert("Local storage is not supported")
+}
